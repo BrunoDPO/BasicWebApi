10023da08e4bdcdc47eb20b0ce97d14e84638076
Bruno Di Prinzio de Oliveira <brunodpo@hotmail.com>
2022-06-16 16:33:25 -0300

    Updated packages and unit test project created

---
+++ tests/BrunoDPO.BasicAPI.Application.UnitTest/Validators/PersonValidatorUnitTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrunoDPO.BasicAPI.Application.UnitTest.Validators
+{
+    public class PersonValidatorUnitTest
+    {
+    }
+}
